0b041381a019060b9b0cba64aade2c70f12d8564
xiaobingchan <1755337994@qq.com>
2019-05-11 22:49:20 +0800

    增加案例题

--- shell-script/部署脚本/php.sh
+++ shell-script/部署脚本/php.sh
@@ -2,10 +2,18 @@
 
 yum install -y gcc libxml2-devel openssl openssl-devel bzip2 bzip2-devel curl-devel libmcrypt libmcrypt-devel mcrypt mhash readline-devel systemtap-sdt-devel
 
+yum remove libzip
+wget https://nih.at/libzip/libzip-1.2.0.tar.gz
+tar -zxvf libzip-1.2.0.tar.gz
+cd libzip-1.2.0
+./configure
+make && make install
+
+cp /usr/bin/python /usr/bin/python2
+cp /usr/local/lib/libzip/include/zipconf.h /usr/local/include/zipconf.h
 wget http://ftp.ntu.edu.tw/pub/php/distributions/php-7.3.5.tar.gz
-
-./configure --prefix=/usr/local/php --with-config-file-path=/etc --enable-inline-optimization --disable-debug --disable-rpath --enable-shared --enable-opcache --enable-fpm --with-fpm-user=www --with-fpm-group=www --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-gettext --enable-mbstring --with-iconv --with-mcrypt --with-mhash --with-openssl --enable-bcmath --enable-soap --with-libxml-dir --enable-pcntl --enable-shmop --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-sockets --with-curl --with-zlib --enable-zip --enable-dtrace --enable-maintainer-zts --with-bz2 --with-readline --without-sqlite3 --without-pdo-sqlite --with-pear
-
+./configure --prefix=/usr/local/php --with-config-file-path=/etc --enable-inline-optimization --disable-debug --disable-rpath --enable-shared --enable-opcache --enable-fpm --with-fpm-user=www --with-fpm-group=www --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-gettext --enable-mbstring --with-iconv --with-mcrypt --with-mhash --with-openssl --enable-bcmath --enable-soap --with-libxml-dir --enable-pcntl --enable-shmop --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-sockets --with-curl --with-zlib --enable-zip --enable-dtrace --enable-maintainer-zts --with-bz2 --with-readline --without-sqlite3 --without-pdo-sqlite --with-pear --with-openssl
+make clean
 make && make install
 cp php.ini-production /usr/local/php/etc/php.ini
 cp /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
@@ -14,5 +22,13 @@ cp /usr/local/php/etc/php-fpm.d/www.conf.default /usr/local/php/etc/php-fpm.d/ww
 /usr/local/php/sbin/php-fpm -t
 cp /data/soft/php-7.3.5/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
 chmod 755 /etc/init.d/php-fpm
-service php-fpm start
+/etc/init.d/php-fpm start
 
+cp /usr/local/php/bin/* /usr/bin/
+
+vi /usr/local/php/etc/php.ini
+extension=openssl
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar  /usr/local/bin/composer
+mv composer.phar  /usr/bin/composer
