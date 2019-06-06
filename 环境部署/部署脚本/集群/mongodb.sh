1a3ab386c1d9bcfc58ddd3dadcafe8728eff0a74
xiaobingchan <1755337994@qq.com>
2019-06-06 15:37:24 +0800

    集群也要会

--- 环境部署/部署脚本/集群/mongodb.sh
+++ 环境部署/部署脚本/集群/mongodb.sh
@@ -1,3 +1,170 @@
 #!/usr/bin/env bash
 
-教程：http://www.ityouknow.com/mongodb/2017/08/05/mongodb-cluster-setup.html
+#教程：http://www.ityouknow.com/mongodb/2017/08/05/mongodb-cluster-setup.html
+
+wget http://downloads.mongodb.org/linux/mongodb-linux-x86_64-rhel70-3.4.21-rc0.tgz
+tar -xzvf mongodb-linux-x86_64-rhel70-3.4.21-rc0.tgz -C /usr/local/
+cd /usr/local/
+mv mongodb-linux-x86_64-rhel70-3.4.21-rc0/ mongodb/
+mkdir -p /usr/local/mongodb/conf
+mkdir -p /usr/local/mongodb/mongos/log
+mkdir -p /usr/local/mongodb/config/data
+mkdir -p /usr/local/mongodb/config/log
+mkdir -p /usr/local/mongodb/shard1/data
+mkdir -p /usr/local/mongodb/shard1/log
+mkdir -p /usr/local/mongodb/shard2/data
+mkdir -p /usr/local/mongodb/shard2/log
+mkdir -p /usr/local/mongodb/shard3/data
+mkdir -p /usr/local/mongodb/shard3/log
+cat >> /etc/profile <<EOF
+export MONGODB_HOME=/usr/local/mongodb
+export PATH=\$PATH:\$MONGODB_HOME/bin
+EOF
+source /etc/profile
+cat > /usr/local/mongodb/conf/config.conf <<EOF
+pidfilepath = /usr/local/mongodb/config/log/configsrv.pid
+dbpath = /usr/local/mongodb/config/data
+logpath = /usr/local/mongodb/config/log/congigsrv.log
+logappend = true
+bind_ip = 0.0.0.0
+port = 21000
+fork = true
+configsvr = true
+replSet=configs
+maxConns=20000
+EOF
+mongod -f /usr/local/mongodb/conf/config.conf
+
+mongo --port 21000
+######################################################################
+config = {
+...    _id : "configs",
+...     members : [
+...         {_id : 0, host : "192.168.56.1:21000" },
+...         {_id : 1, host : "192.168.56.2:21000" },
+...         {_id : 2, host : "192.168.56.3:21000" }
+...     ]
+... }
+rs.initiate(config)
+##########################################################################
+
+
+cat > /usr/local/mongodb/conf/shard1.conf <<EOF
+pidfilepath = /usr/local/mongodb/shard1/log/shard1.pid
+dbpath = /usr/local/mongodb/shard1/data
+logpath = /usr/local/mongodb/shard1/log/shard1.log
+logappend = true
+bind_ip = 0.0.0.0
+port = 27001
+fork = true
+httpinterface=true
+rest=true
+replSet=shard1
+shardsvr = true
+maxConns=20000
+EOF
+mongod -f /usr/local/mongodb/conf/shard1.conf
+
+mongo --port 27001
+##########################################################################
+use admin
+config = {
+...    _id : "shard1",
+...     members : [
+...         {_id : 0, host : "192.168.56.1:27001" },
+...         {_id : 1, host : "192.168.56.2:27001" },
+...         {_id : 2, host : "192.168.56.3:27001" , arbiterOnly: true }
+...     ]
+... }
+rs.initiate(config);
+##########################################################################
+
+
+cat > /usr/local/mongodb/conf/shard2.conf <<EOF
+pidfilepath = /usr/local/mongodb/shard2/log/shard2.pid
+dbpath = /usr/local/mongodb/shard2/data
+logpath = /usr/local/mongodb/shard2/log/shard2.log
+logappend = true
+bind_ip = 0.0.0.0
+port = 27002
+fork = true
+httpinterface=true
+rest=true
+replSet=shard2
+shardsvr = true
+maxConns=20000
+EOF
+mongod -f /usr/local/mongodb/conf/shard2.conf
+mongo --port 27002
+##########################################################################
+use admin
+config = {
+...    _id : "shard2",
+...     members : [
+...         {_id : 0, host : "192.168.56.1:27002"  , arbiterOnly: true },
+...         {_id : 1, host : "192.168.56.2:27002" },
+...         {_id : 2, host : "192.168.56.3:27002" }
+...     ]
+... }
+rs.initiate(config);
+##########################################################################
+
+cat > /usr/local/mongodb/conf/shard3.conf <<EOF
+pidfilepath = /usr/local/mongodb/shard3/log/shard3.pid
+dbpath = /usr/local/mongodb/shard3/data
+logpath = /usr/local/mongodb/shard3/log/shard3.log
+logappend = true
+bind_ip = 0.0.0.0
+port = 27003
+fork = true
+httpinterface=true
+rest=true
+replSet=shard3
+shardsvr = true
+maxConns=20000
+EOF
+mongod -f /usr/local/mongodb/conf/shard3.conf
+mongo --port 27003
+##########################################################################
+use admin
+config = {
+...    _id : "shard3",
+...     members : [
+...         {_id : 0, host : "192.168.56.1:27003" },
+...         {_id : 1, host : "192.168.56.2:27003" , arbiterOnly: true},
+...         {_id : 2, host : "192.168.56.3:27003" }
+...     ]
+... }
+rs.initiate(config);
+##########################################################################
+
+cat > /usr/local/mongodb/conf/mongos.conf <<EOF
+pidfilepath = /usr/local/mongodb/mongos/log/mongos.pid
+logpath = /usr/local/mongodb/mongos/log/mongos.log
+logappend = true
+bind_ip = 0.0.0.0
+port = 20000
+fork = true
+configdb = configs/192.168.56.1:21000,192.168.56.2:21000,192.168.56.3:21000
+maxConns=20000
+EOF
+mongos -f /usr/local/mongodb/conf/mongos.conf
+mongo --port 20000
+##########################################################################
+use  admin
+sh.addShard("shard1/192.168.56.1:27001,192.168.56.2:27001,192.168.56.3:27001")
+sh.addShard("shard2/192.168.56.1:27002,192.168.56.2:27002,192.168.56.3:27002")
+sh.addShard("shard3/192.168.56.1:27003,192.168.56.2:27003,192.168.56.3:27003")
+sh.status()
+db.runCommand( { enablesharding :"testdb"});
+db.runCommand( { shardcollection : "testdb.table1",key : {id: 1} } )
+##########################################################################
+
+mongo  127.0.0.1:20000
+##########################################################################
+use  testdb;
+for (var i = 1; i <= 100000; i++)
+db.table1.save({id:i,"test1":"testval1"});
+db.table1.stats();
+##########################################################################
+
--- 环境部署/部署脚本/集群/mysql_cluster.sh
+++ 环境部署/部署脚本/集群/mysql_cluster.sh
@@ -1,6 +1,81 @@
 #!/usr/bin/env bash
 
-教程：https://cloud.tencent.com/developer/article/1005764
+#教程：https://cloud.tencent.com/developer/article/1005764
 
-mysql-cluster-gpl-7.3.6-linux-glibc2.5-x86_64.tar.gz
+wget http://mysql.mirror.kangaroot.net/Downloads/MySQL-Cluster-7.3/mysql-cluster-gpl-7.3.24-linux-glibc2.12-x86_64.tar.gz
+yum -y install autoconf
+groupadd mysql
+useradd mysql -g mysql
+tar -zxvf mysql-cluster-gpl-7.3.24-linux-glibc2.12-x86_64.tar.gz -C /usr/local/
+cd /usr/local
+mv mysql-cluster-gpl-7.3.24-linux-glibc2.12-x86_64/ mysql/
+chown -R mysql:mysql mysql
 
+su - mysql
+cd /usr/local/mysql
+scripts/mysql_install_db --user=mysql --datadir=/usr/local/mysql/data
+
+# 管理节点
+mkdir -p /var/lib/mysql-cluster
+cd /var/lib/mysql-cluster
+rm -rf config.ini
+cat > config.ini << EOF
+[ndb_mgmd default]
+# Directory for MGM node log files
+DataDir=/var/lib/mysql-cluster
+[ndb_mgmd]
+#Management Node db1
+HostName=192.168.56.1
+[ndbd default]
+NoOfReplicas=1      # Number of replicas
+DataMemory=256M     # Memory allocate for data storage
+IndexMemory=128M    # Memory allocate for index storage
+#Directory for Data Node
+DataDir=/var/lib/mysql-cluster
+[ndbd]
+#Data Node db2
+HostName=192.168.240.114
+[mysqld]
+#SQL Node db3
+HostName=192.168.240.113
+EOF
+chown -R mysql:mysql /var/lib/mysql-cluster
+firewall-cmd --zone=public --add-port=1186/tcp --permanent
+firewall-cmd --reload
+su - mysql
+mkdir -p /usr/local/mysql/mysql-cluster
+/usr/local/mysql/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --initial
+/usr/local/mysql/bin/ndb_mgm
+
+
+mkdir -p /var/lib/mysql-cluster
+chown -R mysql:mysql /var/lib/mysql
+cat > /etc/my.cnf << EOF
+[mysqld]
+ndbcluster
+ndb-connectstring=192.168.240.112
+[mysql_cluster]
+ndb-connectstring=192.168.240.112
+EOF
+firewall-cmd --zone=public --add-port=1186/tcp --permanent
+firewall-cmd --reload
+su - mysql
+/usr/local/mysql/bin/ndbd --initial
+
+
+mkdir -p /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+cat > /etc/my.cnf << EOF
+[mysqld]
+ndbcluster
+ndb-connectstring=192.168.56.1
+[mysql_cluster]
+ndb-connectstring=192.168.56.1
+EOF
+firewall-cmd --zone=public --add-port=1186/tcp --permanent
+firewall-cmd --reload
+cp /usr/local/mysql/support-files/mysql.server /etc/init.d/mysqld
+su - mysql
+/usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --user=mysql --initialize-insecure
+service mysqld start
+/usr/local/mysql/bin/mysqladmin -u root password '12345678'
